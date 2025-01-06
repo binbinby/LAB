522f317146e3b25eecf1c142f54c6f043b362a91
binbinby <napatswon@gmail.com>
2025-01-06 18:04:33 +0700

    no message

--- Code/Cplus/LAB8_08/LAB8_08.cpp
+++ Code/Cplus/LAB8_08/LAB8_08.cpp
@@ -1,20 +1,143 @@
-// LAB8_08.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// กำหนดโครงสร้าง struct
+struct Address {
+	int houseNumber;
+	int villageNumber;
+	string alley;
+	string road;
+	string province;
+	string district;
+	string subDistrict;
+	string postalCode;
+};
+
+struct Student {
+	string idCard;
+	string title;
+	string fristname;
+	string lastname;
+	int mobile;
+	int homePhone;
+	string email;
+	string grade;
+	string school;
+	Address address;
+};
+; // สร้างอาร์เรย์ของ struct ขนาด 5
+struct Guardian {
+	string title;
+	string fristname;
+	string lastname;
+	int mobile;
+	int homePhone;
+	string relation;
+};
+struct StudentInfo {
+	Student student;       // ข้อมูลนักเรียน
+	Guardian guardian;     // ข้อมูลผู้ปกครอง
+};
+
+
+int main() {
+	
+	Address address[1];
+	Student students[1];
+	Guardian guardian[1];
+	// รับข้อมูลนักศึกษา
+	for (int i = 0; i < 1; i++) {
+		cout << "Enter information for student " << i + 1 << ":" << endl;
+		cout << "Student ID: ";
+		cin >> students[i].idCard;
+		cout << "Title: ";
+		cin >> students[i].title;
+		cout << "fristname: ";
+		cin >> students[i].fristname;
+		cout << "lastname: ";
+		cin >> students[i].lastname;
+		cout << "mobile: ";
+		cin >> students[i].mobile;
+		cout << "homePhone: ";
+		cin >> students[i].homePhone;
+		cout << "email: ";
+		cin >> students[i].email;
+		cout << "grade: ";
+		cin >> students[i].grade;
+		cout << "school: ";
+
+		cin >> students[i].school;
+		cout << "Address-house: ";
+		cin >> students[i].address.houseNumber;
+		cout << "Address-villageNumber: ";
+		cin >> students[i].address.villageNumber;
+		cout << "Address-alley: ";
+		cin >> students[i].address.alley;
+		cout << "Address-road: ";
+		cin >> students[i].address.road;
+		cout << "Address-province: ";
+		cin >> students[i].address.province;
+		cout << "Address-district: ";
+		cin >> students[i].address.district;
+		cout << "Address-subDistrict: ";
+		cin >> students[i].address.subDistrict;
+		cout << "Address-postalCode: ";
+		cin >> students[i].address.postalCode;
+	}
+	cout << "Enter information for guardian " << endl;
+	for (int i = 0; i < 1; i++) {
+		cout << "title: ";
+		cin >> guardian[i].title;
+		cout << "fristname: ";
+		cin >> guardian[i].fristname;
+		cout << "lastname: ";
+		cin >> guardian[i].lastname;
+		cout << "mobile: ";
+		cin >> guardian[i].mobile;
+		cout << "homePhone: ";
+		cin >> guardian[i].homePhone;
+		cout << "relation: ";
+		cin >> guardian[i].relation;
+		cout << endl;
+	}
+
+	// แสดงข้อมูลนักศึกษา
+
+	cout << "Student Information:" << endl;
+
+	for (int i = 0; i < 1; i++) {
+		cout << "Student " << i + 1 << ":" << endl;
+		cout << " idCard: " << students[i].idCard << endl;
+		cout << " Title: " << students[i].title << endl;
+		cout << " fristname: " << students[i].fristname << endl;
+		cout << " lastname: " << students[i].lastname << endl;
+		cout << " mobile: " << students[i].mobile << endl;
+		cout << " homePhone: " << students[i].homePhone << endl;
+		cout << " email: " << students[i].email << endl;
+		cout << " grade: " << students[i].grade << endl;
+		cout << " school: " << students[i].school << endl;
+
+		cout << " Address-house: " << students[i].address.houseNumber << endl;
+		cout << " Address-villageNumber: " << students[i].address.villageNumber << endl;
+		cout << " Address-alley: " << students[i].address.alley << endl;
+		cout << " Address-road: " << students[i].address.road << endl;
+		cout << " Address-province: " << students[i].address.province << endl;
+		cout << " Address-district: " << students[i].address.district << endl;
+		cout << " Address-subDistrict: " << students[i].address.subDistrict << endl;
+		cout << " Address-postalCode: " << students[i].address.postalCode << endl;
+	}
+	cout << "Guardian Information:" << endl;
+	for (int i = 0; i < 1; i++) {
+		cout << " Title: " << guardian[i].title << endl;
+		cout << " fristname: " << guardian[i].fristname << endl;
+		cout << " lastname: " << guardian[i].lastname << endl;
+		cout << " mobile: " << guardian[i].mobile << endl;
+		cout << " homePhone: " << guardian[i].homePhone << endl;
+		cout << " relation: " << guardian[i].homePhone << endl;
+		cout << endl;
+	}
+		
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
